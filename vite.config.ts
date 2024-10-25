5d1a3f36f051b491e96fac743426317ab05c58a9
Klonochkin <di630538@yandex.ru>
2024-10-25 11:42:42 +0300

    init

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import browserslistToEsbuild from 'browserslist-to-esbuild';
+import autoprefixer from 'autoprefixer';
+import { join } from 'node:path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    base: './',
+    root: './src',
+    publicDir: '../public',
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+        target: browserslistToEsbuild(),
+        cssTarget: browserslistToEsbuild(),
+    },
+    css: {
+        postcss: {
+            plugins: [autoprefixer()],
+        },
+    },
+    resolve: {
+        alias: {
+            '@': join(import.meta.dirname, './src'),
+        },
+    },
+});
