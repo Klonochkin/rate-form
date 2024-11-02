a75928587f93a384bf51c041a84fd44ed5f6dde5
Klonochkin <di630538@yandex.ru>
2024-11-02 14:18:35 +0300

    refactor: Обновляет react day picker

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -18,31 +18,33 @@ function Calendar({
             showOutsideDays={showOutsideDays}
             className={cn('p-3', className)}
             classNames={{
-                months: 'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0',
                 month: 'space-y-4',
-                month_caption:
-                    'flex justify-center pt-1 relative items-center w-[90%]',
+                month_caption: 'flex justify-center pt-1 relative items-center',
                 caption_label: 'text-sm font-medium',
                 nav: 'space-x-1 flex items-start',
-                button: cn(
+                button_previous: cn(
                     buttonVariants({ variant: 'outline' }),
-                    'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100',
+                    'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute left-4 z-10',
                 ),
-                button_previous: 'absolute left-1',
-                button_next: 'absolute right-1',
+                button_next: cn(
+                    buttonVariants({ variant: 'outline' }),
+                    'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute right-4 z-10',
+                ),
                 month_grid: 'w-full border-collapse space-y-1',
                 weekdays: 'flex',
                 weekday:
                     'text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]',
                 week: 'flex w-full mt-2',
-                day: 'h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
-                day_button: cn(
-                    buttonVariants({ variant: 'ghost' }),
-                    'h-9 w-9 p-0 font-normal aria-selected:opacity-100',
+                day: cn(
+                    'h-9 w-9 text-center text-sm p-0 relative',
+                    'aria-selected:bg-primary',
+                    'aria-selected:text-primary-foreground',
                 ),
-                range_end: 'day-range-end',
+                day_button: 'h-9 w-9 p-0 font-normal aria-selected:opacity-100',
+
                 selected:
-                    'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground',
+                    'bg-primary text-primary-foreground rounded-md hover:bg-primary',
+                range_end: 'day-range-end',
                 day_today: 'bg-accent text-accent-foreground',
                 outside:
                     'day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30',
@@ -51,6 +53,15 @@ function Calendar({
                     'aria-selected:bg-accent aria-selected:text-accent-foreground',
                 hidden: 'h-9 w-9 p-0 font-normal opacity-50 text-[#A3A3A3] pointer-events-none cursor-default',
                 ...classNames,
+            }}
+            modifiersClassNames={{
+                notSelected:
+                    'hover:bg-accent hover:text-accent-foreground rounded-md',
+            }}
+            modifiers={{
+                notSelected: () => {
+                    return true;
+                },
             }}
             components={{
                 Chevron: (props) => {
